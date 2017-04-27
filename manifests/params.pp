2236c8fcff55dcb4bd661b97d7063ff8d1e79bc8
Adam Breaux <breauxaj@gmail.com>
2017-04-27 18:20:52 -0400

    new scaffolding

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,29 +9,40 @@
 class php70::params {
   $php_package_ensure = 'latest'
 
-  case $::operatingsystem {
-    'Amazon': {
-      $php_packages = [
-        'php70',
-        'php70-bcmath',
-        'php70-cli',
-        'php70-common',
-        'php70-dba',
-        'php70-devel',
-        'php70-gd',
-        'php70-intl',
-        'php70-json',
-        'php70-ldap',
-        'php70-mbstring',
-        'php70-mcrypt',
-        'php70-mysqlnd',
-        'php70-pdo',
-        'php70-process',
-        'php70-xml'
-      ]
+  case $::osfamily {
+    'RedHat': {
+      case $::operatingsystem {
+        Amazon: {
+          case $::operatingsystemmajrelease {
+            default: {
+              $php_packages = [
+                'php70',
+                'php70-bcmath',
+                'php70-cli',
+                'php70-common',
+                'php70-dba',
+                'php70-devel',
+                'php70-gd',
+                'php70-intl',
+                'php70-json',
+                'php70-ldap',
+                'php70-mbstring',
+                'php70-mcrypt',
+                'php70-mysqlnd',
+                'php70-pdo',
+                'php70-process',
+                'php70-xml'
+              ]
+            }
+          }
+        }
+        default: {
+          fail("The ${module_name} module is not supported on an ${::operatingsystem} based system.")
+        }
+      }
     }
     default: {
-      fail("The ${module_name} module is not supported on an ${::operatingsystem} based system.")
+      fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
     }
   }
 
